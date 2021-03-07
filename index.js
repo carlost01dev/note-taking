edd756df2362a52eb4cca20fdb12907c27a7ef63
Tu nombre <carlostorrealba@MacBook-Pro-de-Carlos.local>
2021-03-06 22:17:00 -0500

    make it better36

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const moviesRouter = require('./routes/movies.js');
 
 app.use(express.static('public'));
 
-
 mongoose.connect('mongodb://localhost:27017/movies?authSource=admin', {
     keepAlive: true,
     useNewUrlParser: true,
@@ -21,20 +20,6 @@ db.once('open', () => console.log('conected to database'));
 app.use(express.json());
 app.use('/movies', moviesRouter);
 
-const lessons = [
-
-    { id: 1, lesson: 'lesson 1' },
-    { id: 2, lesson: 'lesson 2' },
-    { id: 3, lesson: 'lesson 3' }
-]
-
 app.get('/', (req, res) => res.send('Hello World'));
 
-
-
-
-
-
-
-
-app.listen(process.env.PORT ||3000, () => console.log('Listening port 3000'));
+app.listen(process.env.PORT || 3000, () => console.log('Listening port 3000'));
